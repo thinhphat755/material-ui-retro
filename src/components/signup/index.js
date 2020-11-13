45a4c9fde03a9bce79d0417b582f32ca719f58c7
thinhphat755 <48512210+thinhphat755@users.noreply.github.com>
2020-11-13 11:42:12 +0700

    Added SignUp Component (register user by username and password)

---
+++ src/components/signup/index.js
@@ -0,0 +1,157 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+export default function SignUp() {
+    const classes = useStyles();
+    const [nameText, setNameText] = useState('');
+    const [usernameText, setUsernameText] = useState('');
+    const [passwordText, setPasswordText] = useState('');
+    let history = useHistory();
+
+    const handleNameChange = (e) => {
+        setNameText(e.target.value);
+    }
+
+    const handleUsernameChange = (e) => {
+        setUsernameText(e.target.value);
+    }
+
+    const handlePasswordChange = (e) => {
+        setPasswordText(e.target.value);
+    }
+
+    function handleSubmit(e){
+        fetch("http://localhost:5000/users/register", {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                "name": nameText,
+                "username": usernameText,
+                "password": passwordText
+            })
+        }).then(resp => resp.json())
+            .then(data => {
+                console.log(data);
+                if(data.code === 0){
+                    history.push('/signin');
+                    alert("Registered successfully. You can log in right now!");
+                } else if(data.code === -1){
+                    alert('User already existed. Please choose another one!');
+                } else{
+                    alert('Invalid input!');
+                }
+                
+                //loadData();
+                //setOpen(false);
+            })
+
+        e.preventDefault();
+    }
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                <form method="POST" className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                autoComplete="fname"
+                                name="fullname"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="fullName"
+                                label="Full Name"
+                                onChange={handleNameChange}
+                                autoFocus
+                            />
+                        </Grid>
+                        
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="username"
+                                label="Username"
+                                name="username"
+                                onChange={handleUsernameChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                onChange={handlePasswordChange}
+                            />
+                        </Grid>
+                        {/* <Grid item xs={12}>
+                            <FormControlLabel
+                                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                label="I want to receive inspiration, marketing promotions and updates via email."
+                            />
+                        </Grid> */}
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Sign Up
+                    </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                            <Link to="/signin" variant="body2">
+                                Already have an account? Sign in
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+        </Container>
+    );
+}
