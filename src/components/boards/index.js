5b85997ee9eb27b63edde638e57afff165accb30
thinhphat755 <48512210+thinhphat755@users.noreply.github.com>
2020-11-13 11:52:10 +0700

    Added feature board's sharing to public

--- src/components/boards/index.js
+++ src/components/boards/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import EditBoardDialog from '../editBoardDialog';
-
-//import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-//import Container from '@material-ui/core/Container';
-//import AddIcon from '@material-ui/icons/AddCircle';
-//import AddBoardDialog from '../addBoardDialog';
-//import Box from '@material-ui/core/Box';
-//import './user.css';
 
 const useStyles = makeStyles((theme) => ({
     cardGrid: {
@@ -42,21 +36,33 @@ export default function Board(props) {
     const classes = useStyles();
     const board = props.boardItem;
     const onClickDelete = props.onClickDelete;
-    const onClickBoard = props.onClickBoard;
+    //const onClickBoard = props.onClickBoard;
     const loadData = props.loadData;
 
     const handleDelete = () => {
         onClickDelete(board.id);
     };
 
-    const handleClick = () => {
-        onClickBoard(board);
+    function handleShare(){
+        const id = board.id;
+        fetch(`http://localhost:5000/boards/share/${id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' }
+        }).then(resp => resp.json())
+            .then(data => {
+                if(data.code === 0){
+                    alert('Board has been shared to everyone. Now they can join in and edit with you!');
+                } else{
+                    alert('Ohh! Something went wrong...');
+                }
+            })
     }
 
     return (
         <Grid item xs={12} sm={6} md={4}>
             <Card variant="outlined" className={classes.card}>
-                <CardActionArea onClick={handleClick}>
+                <Link style={{ textDecoration: 'none', color: 'black' }} to={`/boards/${board.id}`}>
+                <CardActionArea>
                     <CardContent className={classes.cardContent}>
                         <Typography gutterBottom variant="h5" component="h2">
                             {board.name}
@@ -66,9 +72,9 @@ export default function Board(props) {
                         </Typography>
                     </CardContent>
                 </CardActionArea>
-
+                </Link>
                 <CardActions >
-                    <Button variant="outlined" size="small" color="primary">
+                    <Button onClick={handleShare} variant="outlined" size="small" color="primary">
                         Share
                     </Button >
                     <Button onClick={handleDelete} variant="outlined" size="small" color="primary">
